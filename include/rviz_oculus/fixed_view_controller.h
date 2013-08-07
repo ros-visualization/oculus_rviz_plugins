fb6ce70305b4d5b2dc513821a41745f7d32963cd
David Gossow <dgossow@willowgarage.com>
2013-08-06 23:11:35 -0700

    added mag calibration

---
+++ include/rviz_oculus/fixed_view_controller.h
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2009, Willow Garage, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef RVIZ_ORBIT_VIEW_CONTROLLER_H
+#define RVIZ_ORBIT_VIEW_CONTROLLER_H
+
+#include <OGRE/OgreVector3.h>
+
+#include <QCursor>
+
+#include "rviz/frame_position_tracking_view_controller.h"
+
+namespace rviz
+{
+class FloatProperty;
+}
+
+namespace rviz_oculus
+{
+
+class FixedViewController: public rviz::FramePositionTrackingViewController
+{
+Q_OBJECT
+public:
+  FixedViewController();
+  virtual ~FixedViewController();
+
+  virtual void onInitialize();
+
+  virtual void reset();
+
+  virtual void update(float dt, float ros_dt);
+
+  virtual void handleMouseEvent(rviz::ViewportMouseEvent& event);
+
+  void zoom( float amount );
+
+private:
+
+  rviz::FloatProperty *fov_property_;
+
+};
+
+}
+
+#endif // RVIZ_VIEW_CONTROLLER_H
